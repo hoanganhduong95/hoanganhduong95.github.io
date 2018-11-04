39a5f4347f6c0a97156e7d0d25016823f4689ca4
hoanganhduong95 <hoanganhduong95@gmail.com>
2018-11-04 12:04:02 +0700

    update abut + backtotop
    back to top loi :/

--- WatchShop/js/main.js
+++ WatchShop/js/main.js
@@ -1,13 +1,30 @@
 // navbar
 
-$(window).scroll(function() {
-    if ($(document).scrollTop() > 100) {
-      $('.navbar').addClass('color-change');
-    } else {
-      $('.navbar').removeClass('color-change');
-    }
-  });
+$(window).scroll(function () {
+  if ($(document).scrollTop() > 100) {
+    $('.navbar').addClass('color-change');
+  } else {
+    $('.navbar').removeClass('color-change');
+  }
+});
+
+//back to top 
+
+var btn = $('#button');
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 500) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function (e) {
+  e.preventDefault();
+  $('html, body').animate({ scrollTop: 0 }, '300');
+});
 
 
-//   ====================
+
 
